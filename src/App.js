f1aa270a80bfbae729d9fef8809a39c95f3ec76d
Joy Goh-Mah <royran9er@gmail.com>
2018-04-15 14:39:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation/>
+        {/* <Logo/>
+        <ImageLinkForm/>
+        <FaceRecognition/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Navigation.css';
+
+class Navigation extends Component {
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Navigation;
