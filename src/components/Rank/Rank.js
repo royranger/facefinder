55e960dab19dadc42d372774d0bd40495e2f75d9
Joy Goh-Mah <royran9er@gmail.com>
2018-08-29 19:27:50 +0100

    updates faces and photo count

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,23 +2,13 @@ import React from 'react';
 
 const Rank = ({name, entries, numFaces}) => {
 
-  let photos = 'photos';
-  if (entries === 1) {
-    photos = 'photo';
-  }
-
-  let faces = 'faces';
-  if (numFaces === 1) {
-    faces = 'face';
-  }
-
   return(
     <div>
       <div className='white f3'>
         {`${name}, you have found`}
       </div>
       <div className='white f1'>
-        {`${numFaces} ${faces} in ${entries} ${photos}!`}
+        {`${numFaces} ${numFaces === '1' ? 'face' : 'faces'} in ${entries} ${entries === '1' ? 'photo' : 'photos'}!`}
       </div>
     </div>
   );
